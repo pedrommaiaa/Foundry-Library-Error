350a2e50e9a9de6eb68b3cd5c20ecce117d7fb24
Pedro Maia <pedrommaiaa@protonmail.com>
2023-02-21 10:03:53 -0500

    Working example

---
+++ src/Helper.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+contract Helper {
+    ERC20 public token;
+
+    constructor(ERC20 _token) { token = _token; }
+
+    function doSafeTransfer(address to, uint256 amount) external {
+        _doSafeTransfer(to, amount);
+    }
+
+    function _doSafeTransfer(address to, uint256 amount) internal {
+        SafeTransferLib.safeTransfer(token, to, amount); 
+    }
+}
--- test/Library.t.sol
+++ test/Library.t.sol
@@ -1,19 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
+import {Helper} from "../src/Helper.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
 contract LibraryTest is Test {
+    Helper public helper;
     MockERC20 public token;
 
     function setUp() public {
         token = new MockERC20("MockToken", "TST", 18);
+        helper = new Helper(token);
     }
 
     function testRevertTransfer() external {
         vm.expectRevert(bytes("TRANSFER_FAILED"));
         SafeTransferLib.safeTransfer(token, address(0xBEEF), 1e18);
+    }
+
+    function testRevertHelperTransfer() external {
+        vm.expectRevert(bytes("TRANSFER_FAILED"));
+        helper.doSafeTransfer(address(0xBEEF), 1e18);
     }
 }
